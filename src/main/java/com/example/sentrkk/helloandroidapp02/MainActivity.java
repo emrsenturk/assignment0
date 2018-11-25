741d579c5fb19d69b87138f00292c83566f16ec7
emrsenturk <sentuurkemre@gmail.com>
2018-11-25 19:49:20 +0100

    Add files via upload
    last update , testing for textview and re-update in savedInstance function

--- src/main/java/com/example/sentrkk/helloandroidapp02/MainActivity.java
+++ src/main/java/com/example/sentrkk/helloandroidapp02/MainActivity.java
@@ -13,17 +13,24 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
+ //save instance state
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+
+        savedInstanceState.putParcelableArrayList("Values",lıstOfCows);
+        super.onSaveInstanceState(savedInstanceState);
         adapter.notifyDataSetChanged();
+
         refreshCounter();
-        super.onRestoreInstanceState(savedInstanceState);
+
     }
 
     private ArrayAdapter<cow> adapter;
@@ -34,9 +41,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        EditText breedField = findViewById(R.id.breedField);
+        EditText idField    = findViewById(R.id.idField) ;
+        Button addButton = findViewById(R.id.button1);
+        Button rejectButton = findViewById(R.id.button2);
+        Button clearButton = findViewById(R.id.button3);
+        ListView listView = findViewById(R.id.listView);
+        TextView headerView = findViewById(R.id.headerView);
+        TextView cowsCounter = findViewById(R.id.cowsCounter);
+        final TextView breedText = findViewById(R.id.editText);
+        TextView idText = findViewById(R.id.editText2);
+
+            //check the state
+        if (savedInstanceState != null) {
+            lıstOfCows = savedInstanceState.getParcelableArrayList("Values") ;
+            refreshCounter();
+
+        }
+
+
         adapter=new ArrayAdapter<cow>(this,android.R.layout.simple_expandable_list_item_1,lıstOfCows);
         ListView lv=findViewById(R.id.listView);
         lv.setAdapter(adapter);
+
+
 
         //clear button
         final Button buttonclear = findViewById(R.id.button3);
@@ -48,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         //add button
         final Button button = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
@@ -55,22 +85,23 @@ public class MainActivity extends AppCompatActivity {
                 EditText breeText = findViewById(R.id.breedField);
                 EditText ıdText = findViewById(R.id.idField);
 
-                //toast message for uygun olmayan input
+
+                //toast message for inappropriate input
                 if (breeText.getText().toString().equals("") && !ıdText.getText().toString().equals("") ||
                         !breeText.getText().toString().equals("") && ıdText.getText().toString().equals("") ||
                          breeText.getText().toString().equals("") && ıdText.getText().toString().equals("")){
 
                     Toast.makeText(getApplicationContext(), "Provide both inputs ", Toast.LENGTH_SHORT).show();
 
-                } else
-                     {
-                cow newCow = new cow(Integer.parseInt(breeText.getText().toString()), Integer.parseInt(ıdText.getText().toString()));
-                lıstOfCows.add(newCow);
-                adapter.notifyDataSetChanged();
-                refreshCounter();
-            }
 
-
+                } else {
+                    if (checkBreed(Integer.parseInt(ıdText.getText().toString()))) {
+                        cow newCow = new cow(Integer.parseInt(breeText.getText().toString()), Integer.parseInt(ıdText.getText().toString()));
+                        lıstOfCows.add(newCow);
+                        adapter.notifyDataSetChanged();
+                        refreshCounter();
+                    }
+                }
             }
         });
 
@@ -86,6 +117,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private boolean checkBreed(int id){
+        for(int i=0;i<this.lıstOfCows.size();i++){
+            if(this.lıstOfCows.get(i).get_ıd()==id)
+                return false;
+        }
+        return true ;
     }
 
     public void refreshCounter (){
